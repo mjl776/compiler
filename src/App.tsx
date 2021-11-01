ff3f2a782bc67e1b3deeed186056ff0d257cde19
YashBit <yashbharti924@gmail.com>
2021-11-01 21:31:05 +0530

    Routing ticket 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.css';
 import HomePage from "./HomePage/Homepage"
-import SignIn from "./authentication/signIn"
+// import SignIn from "./authentication/signIn"
 
 const App = () => {
   return (
     <div className="App">
       <HomePage></HomePage>
-      <SignIn></SignIn>
     </div>
   );
 }
--- src/HomePage/Nav.tsx
+++ src/HomePage/Nav.tsx
@@ -1,21 +1,30 @@
 import React from 'react';
-import { NavLink, BrowserRouter as Router } from "react-router-dom";
+import {Route, NavLink, BrowserRouter as Router } from "react-router-dom";
 import { motion } from 'framer-motion'
 import "./Nav.css"
+import HomePage from "./Homepage"
+import SignIn from "../authentication/signIn"
 const Nav = () => {
     return (
         <div className = "container">
             <Router>
-                <nav>
-                    <ul>
-                        <motion.li whileHover = {{ scale: 1.1 }}> <NavLink to = "/"> Explorer Page</NavLink></motion.li>
-                        <motion.li whileHover = {{ scale: 1.1 }}> <NavLink to = "/Create-Posts"> Posts </NavLink> </motion.li>
-                        <motion.li whileHover = {{ scale: 1.1 }}> <NavLink to = "/Sign-in"> Sign In </NavLink> </motion.li>
-                        <motion.li whileHover = {{ scale: 1.1 }}> <NavLink to = "/Sign-up"> Sign up </NavLink> </motion.li>
-                        <input type = "text" className = "text-box" placeholder = "Search..."/>      
-                    </ul>
-                </nav>
+                <div>
+                    <nav>
+                        <ul>
+                            <motion.li whileHover = {{ scale: 1.1 }}> <NavLink to = "/"> Explorer Page</NavLink></motion.li>
+                            <motion.li whileHover = {{ scale: 1.1 }}> <NavLink to = "/Create-Posts"> Posts </NavLink> </motion.li>
+                            <motion.li whileHover = {{ scale: 1.1 }}> <NavLink to = "/Sign-in"> Sign In </NavLink> </motion.li>
+                            <motion.li whileHover = {{ scale: 1.1 }}> <NavLink to = "/Sign-up"> Sign up </NavLink> </motion.li>
+                            <input type = "text" className = "text-box" placeholder = "Search..."/>      
+                        </ul>
+                        
+
+                    </nav>
+                    <Route path = "/" exact component = {HomePage} />
+                    <Route path = "/Sign-in" exact component = {SignIn} />
+                </div>
             </Router>
+            
         </div>
     );
 }
