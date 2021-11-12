2c68fc68f7489c99e2616ea82ccbcdb06f5a9a5e
YashBit <yashbharti924@gmail.com>
2021-11-12 21:53:35 +0530

    Working on Firebase - current error

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import {collection, getDocs} from "firebase/firestore"
+import {db} from "./firebase/firebase"
+function App() {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() =>{
+        // Asynchronous Function from the API Promise. Promise = Binary Result of the API Call
+
+        const usersCollectionRef = collection(db, "posts");
+        const getUsers = async () => {
+            // Logic from getting the users from FireBase
+            const data = await getDocs(usersCollectionRef);
+            console.log(data);
+
+            // Error above, but we still write the skeleton code
+
+            setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+
+        }
+        getUsers();
+    }, [])
+
+
+    return <div className ="App"> 
+    <input placeholder="Name ..." />
+    {users.map((user) => {
+        return (
+        <div>
+            {" "}
+            <h1>{user.name}</h1>
+        </div>
+        );
+    })}
+    </div>
+}
+
+export default App;
