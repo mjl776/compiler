631dd745cf144c2126d2658285b054cdb55afc75
Michael Lee <mlee@ms-mbp-2.myfiosgateway.com>
2021-12-04 02:00:12 -0500

    fixed firebase bug

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import 'firebase/storage';
 import { getFirestore } from "@firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -21,7 +22,13 @@ const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
 
-// Initialize Firebase
-export default db;
+
+// export 
 export const auth = getAuth(app);
+
+export const storage = getStorage(app);
+
+export default db;
+
+
 
