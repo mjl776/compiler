ad97d928ed6a5ec9adcc18d530846417e90621ff
Michael Lee <mlee@10-16-4-83.dynapool.wireless.nyu.edu>
2021-12-10 21:21:13 -0500

    addded job and profile description field

--- src/authentication/signUp.tsx
+++ src/authentication/signUp.tsx
@@ -25,19 +25,12 @@ const SignUp = () => {
   const [newGitHub, setGitHub] = useState("");
   const [newLinkedIn, setLinkedIn] = useState("");
   const [newInstagram, setInstagram] = useState("");
+  const [newProfileDescription, setProfileDescription] = useState("");
+  const [newJob, setJob] = useState("");
   const usersCollectionRef = collection(db, "users");
-  const [file, setFile] = useState(null);
-  const [fileName, setFileName] = useState("");
-  const [userID, setUserID]: any = useState(null);
-  const [url, setUrl]: any = useState(null);
   var history = useHistory();
 
 // File upload
-const onFileChange = async (event: any) => {
-  const file = event.target.files[0];
-  setFile(file);
-  setFileName(file.name);
-};
 
   const register = async () => {
     try {
@@ -47,46 +40,15 @@ const onFileChange = async (event: any) => {
         registerPassword
       ).then(async cred => {
         const post = await addDoc(usersCollectionRef, {username: newUserName, linkedin: newLinkedIn, 
-        github: newGitHub, instagram: newInstagram, user_id: cred.user.uid, profileURL: ""})
+        github: newGitHub, instagram: newInstagram, user_id: cred.user.uid, profile_description: newProfileDescription, job: newJob})
+      }).then(() => {
+        history.push("/");
       });
 
     } catch (error: any) {
       console.log(error.message);
     }
   };
-
-  const photo_db_post = async(uid: any, id: any ) => {
-    if (!file) return;
-    // Listen for state changes, errors, and completion of the upload.
-    const storageRef = ref(storage, "profile-photos/" + uid + "/" + "profile_pic" + "/" + fileName);
-    const uploadFile = uploadBytesResumable(storageRef, file);
-    uploadFile.on('state_changed',
-            (snapshot) => {
-            // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-            console.log('Upload is ' + progress + '% done');
-            switch (snapshot.state) {
-                case 'paused':
-                console.log('Upload is paused');
-                break;
-                case 'running':
-                console.log('Upload is running');
-                break;
-            }
-            }, 
-            (error) => {
-                console.log(error);
-            }, 
-            async () => {
-                await getDownloadURL(uploadFile.snapshot.ref).then((downloadURL)=> {
-                  setUrl(downloadURL);
-                }).then(() => {
-                    history.push("/");
-                });
-            },
-
-        );
-  }
 
     return (
       <div>
@@ -97,14 +59,6 @@ const onFileChange = async (event: any) => {
                 Welcome to the sign up page!
           </div> 
 
-            <div className = "create-photo-slogan">
-                Insert any profile photo Below: 
-            </div> 
-
-            <div>
-                <input type="file" className="input"  onChange = {onFileChange}/>
-            </div>
-
           <div className = "username-box-outside-border">
             <input type = "text" className = "username-box" placeholder = "email..." 
             
@@ -114,6 +68,8 @@ const onFileChange = async (event: any) => {
             
             />
           </div>
+        
+
           <div className = "password-box-outside-border">
             <input type = "text" className = "password-box" placeholder = "password..."
             
@@ -122,6 +78,7 @@ const onFileChange = async (event: any) => {
             }}
             />
           </div>
+
           <div className = "username-box-outside-border">
             <input type = "text" className = "username-box" placeholder = "username..."
             
@@ -132,6 +89,18 @@ const onFileChange = async (event: any) => {
             
             />
           </div>
+          
+          <div className = "text-field-profile">
+                <textarea
+                    placeholder="   Profile Description"
+                    onChange = {(event) =>{
+                        setProfileDescription(event.target.value);
+                    }}
+                    className = "text-area-for-posts"
+                >
+                </textarea>
+            </div>
+
           <div className = "username-box-outside-border">
             <input type = "text" className = "username-box" placeholder = "Github"
             onChange={(event) => {
@@ -160,6 +129,17 @@ const onFileChange = async (event: any) => {
             
             />
           </div>
+
+          <div className = "username-box-outside-border">
+            <input type = "text" className = "username-box" placeholder = "Job Title..." 
+            
+            onChange={(event) => {
+              setJob(event.target.value);
+            }}
+            
+            />
+          </div>
+
           <div className = "signup-button-outside-border">
               <motion.button whileHover = {{ scale: 1.1 }} className = "signup-button" onClick={register}>
                   Sign up
